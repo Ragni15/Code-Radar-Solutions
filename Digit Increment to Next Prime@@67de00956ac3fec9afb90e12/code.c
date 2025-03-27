726feac5564b527ba55e95be1cb3fb14b8cf95fb
Ragni Kapoor <kapoorragni16@gmail.com>
2025-03-27 19:12:38 +0530

    Solution to Digit Increment to Next Prime

---
+++ Digit Increment to Next Prime@@67de00956ac3fec9afb90e12/code.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int nextPrimeDigit(int digit) {
+    switch (digit) {
+        case 0:
+        case 1:
+        case 8:
+        case 9:
+            return 2;
+        case 2:
+        case 3:
+        case 4:
+            return 3;
+        case 5:
+        case 6:
+        case 7:
+            return 7;
+        default:
+            return -1; 
+    }
+}
+int incrementToPrimeDigits(int N) {
+    int result = 0, multiplier = 1;
+    while (N > 0) {
+        int digit = N % 10; 
+        int primeDigit = nextPrimeDigit(digit); 
+        result += primeDigit * multiplier;
+        multiplier *= 10; 
+        N /= 10; /
+    }
+    return result;
+}
+int main() {
+    int N;
+    scanf("%d", &N);
+    int result = incrementToPrimeDigits(N);
+    printf("%d\n", result);
+    return 0;
+}
